db0707076b434292419f51e28183587ee467efc1
CharizardFire58 <jd.maceirasg@gmail.com>
2020-09-09 23:59:57 -0400

    varios ajustes a algunos archivos

--- comandos/testsound.js
+++ comandos/testsound.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const Discord = require("discord.js")
-
 exports.run = async (client, message) => {
     client.playAudio('./media/audio/mus_house1.ogg', message);
 }
--- eventos/ready.js
+++ eventos/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
     client.logger.log("info", `[READY] ${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`, "ready");
-    client.user.setActivity('with my bot friends!');
+    client.user.setActivity('PSw4 Beta 1.0.0');
 };
