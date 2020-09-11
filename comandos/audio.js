b088898646a1a87b57b8956141fc605d20778c36
CharizardFire58 <jd.maceirasg@gmail.com>
2020-09-10 21:49:59 -0400

    convertir el comando testsound en un comando completo para reproducir diversos audios

---
+++ comandos/audio.js
@@ -0,0 +1,25 @@
+const { botowner } = require('../config.json');
+
+exports.run = async (client, message, args) => {
+    const voz = message.member.voice.channel;
+
+    switch (args[0]) {
+
+        // Aca se pondran todos los audios que reproduzca el bot.
+        case 'sousa':
+            client.playAudio('./media/audio/sousa.mpeg', message);
+            break;
+
+        // Solo el dueño del bot puede detener un audio
+        case 'stop':
+            if (message.author.id !== botowner) return message.reply('no tienes el permiso para hacer esto.');
+            voz.leave();
+            break;
+
+        // Cuando no se ponga el argumento, saltar este mensaje (proximamente con mas informacion)
+        default:
+            message.reply('especifica que audio quieres que reproduzca.');
+    }
+
+
+}
